e9ea8aabac094ded4958d012ff65a5298b3614de
Annika Lang <Lang-annika@t-online.de>
2020-05-08 22:18:32 +0200

    corrected file extension to .py, added CreatePlaylist class and (empty) method definition to each step

---
+++ create_playlist.py
@@ -0,0 +1,27 @@
+# 1. Log into YouTube
+# 2. Grab liked videos
+# 3. Create a new playlist on spotify
+# 4. Search for the song
+# 5. Add this song to the new spotify playlist
+
+class CreatePlaylist:
+
+# 1. Log into YouTube
+  def __init__(self):
+    pass
+
+# 2. Grab liked videos
+  def get_youtube_client(self):
+    pass
+
+# 3. Create a new playlist on spotify
+  def create_playlist(self):
+    pass
+
+# 4. Search for the song
+  def get_spotify_uri(self):
+    pass
+
+# 5. Add this song to the new spotify playlist
+  def add_song_to_playlist(self):
+    pass
